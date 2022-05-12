59e69ffb37a19cf19c55dd015bbc8c5813ccb2db
xuvzduozg <103233511+XuanDuong01@users.noreply.github.com>
2022-05-13 04:18:36 +0700

    upload

---
+++ mini-projects/MatchwornStore/assets/js/product-detail-change-image.js
@@ -0,0 +1,17 @@
+var ImageSlideItem= document.querySelectorAll(".product-image-slide-item");
+
+
+document.querySelector(".product-image-showed")
+
+ImageSlideItem.forEach(function(item){
+
+    item.onclick= function(e){
+        if(document.querySelector(".product-image-slide-item.slide-item-selected")){
+            document.querySelector(".product-image-slide-item.slide-item-selected").classList.remove("slide-item-selected");
+            
+        }
+        e.target.classList.toggle("slide-item-selected");
+        document.querySelector(".product-image-showed img").src=e.target.src;
+        
+    }
+});
