811863871076c8f85163c56e9a9176f763c96052
xuvzduozg <103233511+XuanDuong01@users.noreply.github.com>
2022-05-16 23:16:41 +0700

    upload

--- mini-projects/MatchwornStore/assets/js/index.js
+++ mini-projects/MatchwornStore/assets/js/index.js
@@ -55,17 +55,38 @@ fetch(jerseyApi)
     });
     // đếm số sản phẩm
 
-    if(jerseys.length==0){
-        document.querySelector(".amount-jerseys-line").innerText="There are no jerseys";
-        document.querySelector(".amount-jerseys-line").style.marginBottom="300px";
-    }
-    else if(jerseys.length==1){
+    if(document.querySelectorAll(".show-products-item").length==1){
         document.querySelector(".amount-jerseys-line").innerText="There is a jersey";
+        document.querySelector(".amount-jerseys-line").style.marginBottom="12px";
     }
-    else if(jerseys.length>1){
+    else if(document.querySelectorAll(".show-products-item").length>1){
         document.querySelector(".amount-jerseys-line").innerText=`There are ${jerseys.length} jerseys`;
+        document.querySelector(".amount-jerseys-line").style.marginBottom="12px";
     }
+    
 })
 
 
+if(document.querySelectorAll(".show-products-item").length==0){
+    document.querySelector(".amount-jerseys-line").innerText="There are no jerseys";
+    document.querySelector(".amount-jerseys-line").style.marginBottom="300px";
+}
 
+// var productItemImage = document.querySelector(".product-item-image");
+// console.log(productItemImage.style.width); 
+// alert(productItemImage.style.width);
+
+var serviceItemButton = document.querySelectorAll(".service-item h5");
+
+serviceItemButton.forEach(function(item,index){
+    item.onclick=function(e){
+        if(index!=2){
+            e.target.classList.toggle("service-item-selected");
+            if( e.target.parentNode.querySelector(".service-item-selected"))
+                e.target.parentNode.lastElementChild.style.display="block";
+            else
+                e.target.parentNode.lastElementChild.style.display="none";
+        }
+        
+    }
+});
