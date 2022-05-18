8ad856e99e942582c026eea41f42cfa7a1f8c88e
xuvzduozg <103233511+XuanDuong01@users.noreply.github.com>
2022-05-18 16:39:51 +0700

    upload

--- mini-projects/MatchwornStore/assets/js/my-account.js
+++ mini-projects/MatchwornStore/assets/js/my-account.js
@@ -4,5 +4,14 @@ var inputEmpty = accOverviewLines.forEach(function(item){
     console.log(item.lastElem);
 });
 
+if(document.querySelector(".mini-menu-button-open")){
+    document.querySelector(".header-section").style.position="fixed";
+}
+else{
+    document.querySelector(".header-section").style.position="relative";
+}
+
+
+
 
 
