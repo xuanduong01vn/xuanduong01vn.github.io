4d0de73abedf69f8705fac9e278c45cb9066f388
Xuan Duong <103233511+xuanduong01vn@users.noreply.github.com>
2022-08-18 13:55:57 +0700

    Delete miniprojects/viblo-post-system/server directory

--- miniprojects/viblo-post-system/server/routers/authenticate.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import authenticateControllers from '../controllers/authenticateController.js'
-import loginControllers from '../controllers/loginController.js';
-const router = express.Router();
-
-router.get('/',loginControllers.authenticateUser);
-
-export default router;
