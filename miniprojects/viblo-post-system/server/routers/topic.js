94784149713909caa6c60247aaf842d6028a8250
duongnx <duong.nx@zinza.com.vn>
2022-12-01 09:57:11 +0700

    update avatar

--- miniprojects/viblo-post-system/server/routers/topic.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import topicControllers from '../controllers/topicController.js'
-const router = express.Router();
-
-router.get('/',topicControllers.getTopics);
-
-export default router;
